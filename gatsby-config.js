dc0e4a5ae78598cc17e2e2e763a7058a48152d6c
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2017-11-08 21:06:27 -0500

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-96915741-1',
+        // Setting this parameter is optional
+        anonymize: false,
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
