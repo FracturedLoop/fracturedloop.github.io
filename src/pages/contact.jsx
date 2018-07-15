cf0accd74a213e84e140e80c9a44489859b6a993
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-07-14 21:03:26 -0400

    Add contact page content

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import '../css/helpers.scss';
+
 const ContactPage = () => (
   <div>
-    <h1>Contact</h1>
+    <div className="modern-header">
+      <h1>So you want to get in contact with me, eh?</h1>
+    </div>
     <p>
-      Welcome to the contact page! Things aren't finished here yet, so... be
-      sure to check back soon.
+      There’s a couple of ways you can contact me, but the easiest is probably
+      via <a href="mailto:josiah.nunemaker@gmail.com">email</a>.
     </p>
-    For now, you might want to <Link to="/">go back to the homepage</Link>
+    <p>
+      You can shoot me an email at{' '}
+      <a href="mailto:josiah.nunemaker@gmail.com">josiah.nunemaker@gmail.com</a>.
+    </p>
+    Didn’t want to contact me? Okay, feel free to{' '}
+    <Link to="/">go back to the homepage</Link> then.
   </div>
 );
 
