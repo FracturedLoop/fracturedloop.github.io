9f20b1f36d00733f9341157e9fc626f9c2fd2c53
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-07-14 21:03:09 -0400

    Add about page content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import '../css/helpers.scss';
+
 const AboutPage = () => (
   <div>
-    <h1>About</h1>
+    <div className="modern-header">
+      <h2>Hey there,</h2>
+      <h1>I’m a full-stack web developer.</h1>
+    </div>
+    I tinker with all sorts of things, but my passion is building rich user
+    interfaces.
     <p>
-      Welcome to the about page! Things aren’t finished here yet, so... be sure
-      to check back soon.
+      <b>React</b> is my friend :)
     </p>
-    For now, you might want to <Link to="/">go back to the homepage</Link>
+    <p>
+      Oh, and I also use <b>GraphQL</b>, or, more particularly, <b>Apollo</b>.
+    </p>
+    <p>
+      <b>Node.js</b> with <b>Express</b>, too.
+    </p>
+    <p>
+      I also tinker with <b>MySQL</b>.
+    </p>
+    <p>
+      Feel free to check out some of my{' '}
+      <a href="https://github.com/josnun">projects</a>
+    </p>
+    <h4>
+      <Link to="/">go back to the homepage</Link>
+    </h4>
   </div>
 );
 
